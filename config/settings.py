32fb6cbc4416456bca0be0fd4b2b1109b24a43d0
Yaroslav <20011aric@gmail.com>
2026-08-15 11:58:12 +0300

    Fix STATIC_ROOT for Render

--- config/settings.py
+++ config/settings.py
@@ -91,5 +91,6 @@ USE_TZ = False
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / 'static' ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
