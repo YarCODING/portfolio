fb65f86003ccf62c1a7845bf510e46930cff0826
Yaroslav <20011aric@gmail.com>
2026-08-15 17:06:07 +0300

    added ping url

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'portfolio/index.html')
+
+def ping(request):
+    return HttpResponse("pong", content_type="text/plain")
